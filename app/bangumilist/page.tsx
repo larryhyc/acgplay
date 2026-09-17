272054280acdff2b5e777bb25bdd064fec43f79f
larryhyc <hyc157981310@gmail.com>
2026-09-17 23:28:35 +0800

    fix:修改登录页面的css

--- app/bangumilist/page.tsx
+++ app/bangumilist/page.tsx
@@ -1,4 +1,23 @@
+'use client';
+
+import { useSession } from 'next-auth/react';
+import { useRouter } from 'next/navigation';
+import { useEffect } from 'react';
+
 const BangumiList = () => {
+  const { data: session, status } = useSession();
+  const route = useRouter();
+
+  useEffect(() => {
+    if (!session) {
+      route.push('/login');
+    }
+  }, [session, status, route]);
+
+  if (!session) {
+    return null;
+  }
+
   return <div>追番页面</div>;
 };
 
